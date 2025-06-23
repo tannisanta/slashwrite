d11015a3e018b4eb26d51c3c625b6965f3cc9d7a
Tania Tsakun <tetiana.tsakun@solidgate.com>
2025-06-23 23:35:38 +0200

    Merge due to hone page update

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
-["src/content/blog/cloudflare-pages-deployment-guide-zh.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcloudflare-pages-deployment-guide-zh.mdx&astroContentModuleFlag=true")],
+["src/content/projects/modern-blog-template.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fmodern-blog-template.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/getting-started-with-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgetting-started-with-astro.mdx&astroContentModuleFlag=true")],
-["src/content/projects/modern-blog-template.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fmodern-blog-template.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/cloudflare-pages-deployment-guide-zh.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcloudflare-pages-deployment-guide-zh.mdx&astroContentModuleFlag=true")]]);
 		
