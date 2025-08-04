9386d8e6bbfe3aa3aad19b80a61512775b2c31bf
Tetiana Tsakun <112710487+tannisanta@users.noreply.github.com>
2025-08-04 02:34:47 +0200

    update links

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,11 +1,8 @@
 
 export default new Map([
-["src/content/canvas/quotes-fav.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcanvas%2Fquotes-fav.mdx&astroContentModuleFlag=true")],
-["src/content/blog/how-i-became-a-technical-writer.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhow-i-became-a-technical-writer.mdx&astroContentModuleFlag=true")],
+["src/content/blog/mkdocs-website.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmkdocs-website.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/modern-blog-template.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fmodern-blog-template.mdx&astroContentModuleFlag=true")],
-["src/content/blog/new-blog.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fnew-blog.mdx&astroContentModuleFlag=true")],
-["src/content/canvas/through-lens.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcanvas%2Fthrough-lens.mdx&astroContentModuleFlag=true")],
-["src/content/canvas/through-lens copy.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcanvas%2Fthrough-lens+copy.mdx&astroContentModuleFlag=true")],
 ["src/content/canvas/timless-wisdom.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcanvas%2Ftimless-wisdom.mdx&astroContentModuleFlag=true")],
-["src/content/blog/mkdocs-website.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmkdocs-website.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/how-i-became-a-technical-writer.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhow-i-became-a-technical-writer.mdx&astroContentModuleFlag=true")],
+["src/content/canvas/through-lens.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcanvas%2Fthrough-lens.mdx&astroContentModuleFlag=true")]]);
 		
