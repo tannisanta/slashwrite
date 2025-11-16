212e11a7fdb3d8010d3fc6a2d656b7882e388048
Tetiana Tsakun <112710487+tannisanta@users.noreply.github.com>
2025-11-16 19:05:33 +0100

    New article

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,10 +1,11 @@
 
 export default new Map([
-["src/content/blog/mkdocs-website.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmkdocs-website.mdx&astroContentModuleFlag=true")],
-["src/content/projects/html-css-project.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fhtml-css-project.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/how-i-became-a-technical-writer.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhow-i-became-a-technical-writer.mdx&astroContentModuleFlag=true")],
-["src/content/projects/owlyreads-api-project.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fowlyreads-api-project.mdx&astroContentModuleFlag=true")],
+["src/content/blog/mkdocs-website.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmkdocs-website.mdx&astroContentModuleFlag=true")],
+["src/content/blog/writing technically: skills and tools.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fwriting+technically%3A+skills+and+tools.mdx&astroContentModuleFlag=true")],
+["src/content/projects/html-css-project.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fhtml-css-project.mdx&astroContentModuleFlag=true")],
 ["src/content/canvas/through-lens.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcanvas%2Fthrough-lens.mdx&astroContentModuleFlag=true")],
+["src/content/projects/owlyreads-api-project.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fowlyreads-api-project.mdx&astroContentModuleFlag=true")],
 ["src/content/canvas/timless-wisdom.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcanvas%2Ftimless-wisdom.mdx&astroContentModuleFlag=true")],
 ["src/content/canvas/through-watercolor.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fcanvas%2Fthrough-watercolor.mdx&astroContentModuleFlag=true")]]);
 		
